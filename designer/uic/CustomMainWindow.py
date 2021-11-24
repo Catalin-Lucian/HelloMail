d6118cfe151339e35c02d43b1ddea277e7f4c04e
cezar-plesu <79512221+cezar-plesu@users.noreply.github.com>
2021-11-24 08:31:18 +0200

    Revert "test button"
    This reverts commit 89c6355042b0a4fcf5625eea06b9a9490bc5435c.

--- designer/uic/CustomMainWindow.py
+++ designer/uic/CustomMainWindow.py
@@ -5,8 +5,8 @@ class CustomMainWindow(Ui_MainWindow):
 
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
-        # make comenctions
-        self.testPushButton.clicked.connect(self.test)
+        # # make comenctions
+        # self.testPushButton.clicked =
 
     def test(self):
         print('-' * 50)
