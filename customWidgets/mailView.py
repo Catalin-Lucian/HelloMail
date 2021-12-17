24407504e5f4f73c9017367b2d1cbe5b81d47b2b
Lucian <63058063+Catalin-Lucian@users.noreply.github.com>
2021-12-17 22:46:50 +0200

    better mailview

--- customWidgets/mailView.py
+++ customWidgets/mailView.py
@@ -1,13 +1,20 @@
-from PyQt5.QtCore import QRect, QSize, Qt
+from PyQt5.QtCore import QRect, QSize, Qt, QPoint
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QFrame
+from PyQt5.QtWidgets import QFrame, QLabel
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+from customWidgets.avatarIcon import AvatarIcon
 
 
 class MailView(QFrame):
     def __init__(self, container):
         super(MailView, self).__init__(container)
         self.mailContentView = QWebEngineView(self)
+        self.avatarIcon = AvatarIcon(self)
+        self.senderNameLabel = QLabel(self)
+        self.senderEmailLabel = QLabel(self)
+        self.dateTimeLabel = QLabel(self)
+        self.subjectLabel = QLabel(self)
 
         self.setupUi()
 
@@ -23,33 +30,60 @@ class MailView(QFrame):
                                            "border:10px solid black;"
                                            "border-radius:10px};\n")
         self.mailContentView.page().setBackgroundColor(Qt.transparent)
-        # font = QFont()
-        # font.setFamily("Calibri")
-        # font.setPointSize(50)
-        # self.mailContentView.setFont(font)
 
-        html = """
-               <html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><title>AntenaPlay</title><meta name="description" content=""></head><body style="margin: 0;">
-               <div style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; background-color: #141414; margin: 0 auto; width: 550px; padding: 0 100px 100px; color: #888; font-size: 18px; line-height: 1.44; font-weight: 300;">
-               <img src="https://antenaplay.ro/img/shapes.png" style="float: right; margin-right: -20px; margin-bottom: -18px;"><a href="https://tr.nl.aplay.ro/tr/rot05219EaCzmguGrdcuxQMYovrWD21/f_nagrancynl.eb" target="_blank"><img src="https://antenaplay.ro/img/antenaplaylogo.png" style="margin-bottom: 55px;"></a>
-               <p style="margin: 0 0 26px;">Salut rewards,</p>
-               <p style="margin: 0 0 26px;">Ai întâmpinat probleme la plata abonamentului? Consultă secțiunea de întrebări frecvente pentru a afla toate detaliile legate de abonament <a href="https://tr.nl.aplay.ro/tr/rot0521BCULocL9knjAoAKTm3ptWD21/f_nwhgbe.nagrancynl.eb" target="_blank" style="color: #00A1E4; text-decoration: none;">ajutor.antenaplay.ro</a></p>
-               <a href="https://tr.nl.aplay.ro/tr/rot0521Zx3DujW9pL4Niq3iRefuWD21/f_nagrancynl.eb/pbag/nobanzrag?u=o65p937113p26093nq8R" target="_blank" style="background-color: #00A1E4; text-transform: uppercase; font-weight: bold; color: #fff; text-decoration: none; padding: 15px 30px; border-radius: 50px; display: inline-block; letter-spacing: 1px; margin: 10px 0 0 -2px; font-size: 17px;">Înapoi la pagina abonament </a>
-               <div style="height: 36px;"></div>        
-               <img src="https://antenaplay.ro/email/opens/10689310/b65c937113c26093ad8E" width="1" height="1"><p style="margin: 0 0 26px;">Te aşteaptă 9 canale LIVE și peste 100.000 de ore de conținut din emisiunile și serialele tale preferate.</p>
-               <p style="margin: 0 0 26px;"><strong>Mulţumim,<br>Echipa AntenaPLAY</strong></p>
-               <p style="margin: 56px 0 26px; font-size: 14px;">AntenaPlay.ro este un serviciu oferit de Antena TV Group SA, București</p>
-               </div>
-               <img src="https://tr.nl.aplay.ro/tr/v/40aa04a4-b9c0-11eb-8aed-666ccb90f87e" width="1" height="1" alt="" />
-               </body></html>
-               """
-        self.setMailContentView(html)
+        self.avatarIcon.setGeometry(QRect(30, 30, 50, 50))
+        self.avatarIcon.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+                                      "border: 0px solid rgb(199, 199, 199);\n"
+                                      "border-radius: 25px;")
+        self.avatarIcon.hide()
 
-    def setMailContentView(self, mailDataContent):
-        self.mailContentView.setHtml(mailDataContent)
+        self.senderNameLabel.setGeometry(QRect(100, 20, 275, 30))
+        font = QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
+        self.senderNameLabel.setFont(font)
+        self.senderNameLabel.setStyleSheet("color: rgb(255, 255, 255);")
+
+        self.senderEmailLabel.setGeometry(QRect(100, 50, 275, 30))
+        font = QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setWeight(50)
+        self.senderEmailLabel.setFont(font)
+        self.senderEmailLabel.setStyleSheet("color: rgb(255, 255, 255);")
+
+        self.dateTimeLabel.setGeometry(QRect(403, 35, 135, 20))
+        font = QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(10)
+        self.dateTimeLabel.setFont(font)
+        self.dateTimeLabel.setStyleSheet("color: rgb(255, 255, 255);")
+
+        self.subjectLabel.setGeometry(QRect(20, 100, 705, 35))
+        font = QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(18)
+        self.subjectLabel.setFont(font)
+        self.subjectLabel.setAlignment(Qt.AlignCenter)
+        self.subjectLabel.setStyleSheet("color: rgb(255, 255, 255);")
+        self.subjectLabel.setScaledContents(True)
+
+    def setMailContentView(self, mailData):
+        if mailData.get("content"):
+            self.mailContentView.setHtml(mailData["content"])
         self.mailContentView.page().setBackgroundColor(Qt.transparent)
+        self.avatarIcon.show()
+        self.senderNameLabel.setText(mailData["fromName"])
+        self.senderEmailLabel.setText(mailData["fromEmail"])
+        self.dateTimeLabel.setText(mailData["date"])
+        self.subjectLabel.setText(mailData["subject"])
 
     def resizeContent(self, e: QSize):
-        self.resize(QSize(self.size().width()+e.width(), self.size().height()+e.height()))
-        self.mailContentView.resize(self.mailContentView.size().width()+e.width(),
-                                    self.mailContentView.size().height()+e.height())
+        self.resize(QSize(self.size().width() + e.width(), self.size().height() + e.height()))
+        self.mailContentView.resize(self.mailContentView.size().width() + e.width(),
+                                    self.mailContentView.size().height() + e.height())
+        self.dateTimeLabel.move(QPoint(self.dateTimeLabel.pos().x()+e.width(), self.dateTimeLabel.pos().y()))
+        self.subjectLabel.resize(QSize(self.subjectLabel.size().width()+e.width(), self.subjectLabel.size().height()))
