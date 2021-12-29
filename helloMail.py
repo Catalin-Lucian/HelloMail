e3f0a83c80da3f1275e2b3371d559e1aff574fc5
Lucian <63058063+Catalin-Lucian@users.noreply.github.com>
2021-12-29 23:13:09 +0200

    ??

--- helloMail.py
+++ helloMail.py
@@ -3,9 +3,11 @@ import sys
 from PyQt5.QtCore import QSize, QPoint
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtCore, QtWidgets, QtGui
+
 from customWidgets.mailList import MailList
 from customWidgets.mailView import MailView
 from module.gmailApiService import GoogleApi
+from module.settingsConfig import SettingsConfig
 
 API_NAME = 'gmail'
 API_VERSION = 'v1'
@@ -26,6 +28,9 @@ class HelloMail(QMainWindow):
         self.mailView = MailView(self.centralWidget)
         self.mailCover = QtWidgets.QFrame(self.centralWidget)
 
+        self.settings = SettingsConfig()
+        self.settings.subscribe(self)
+
         self.setupUi()
         self.addMailItemsOnStartUp()
 
@@ -33,12 +38,11 @@ class HelloMail(QMainWindow):
         self.setWindowTitle("HelloMail")
         self.resize(1440, 900)
         self.setMinimumSize(QtCore.QSize(1440, 900))
-        self.setStyleSheet("background-color: rgb(24, 29, 35);")
+        self.setStyleSheet(self.settings.getStyleSheet("mainWindow"))
         self.setCentralWidget(self.centralWidget)
 
         self.mailCover.setGeometry(QtCore.QRect(244, 830, 432, 81))
-        self.mailCover.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, "
-                                     "stop:0.4 rgba(24, 29, 35, 255), stop:1 rgba(255, 255, 255, 0));")
+        self.mailCover.setStyleSheet(self.settings.getStyleSheet("mailCover"))
         self.mailCover.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.mailCover.setFrameShadow(QtWidgets.QFrame.Raised)
 
@@ -78,6 +82,10 @@ class HelloMail(QMainWindow):
         if 'UNREAD' in mailItem.mailData['labelIds']:
             self.googleApi.modify_labels_to_email(mailItem.mailData.get('id'), [], ['UNREAD'])
             mailItem.mailData['labelIds'].remove('UNREAD')
+
+    def notify(self):
+        # ---------------------- get notification from settings -----------------
+        pass
 
 
 if __name__ == '__main__':
---
+++ module/settingsConfig.py
@@ -0,0 +1,39 @@
+import json
+
+
+class SettingsConfig:
+    def __init__(self):
+        self.subs = []
+        self.theme = self.initTheme("default.json")
+
+    def initTheme(self, filename):
+        with open(f"customWidgets/styles/{filename}") as file_object:
+            # store file data in object
+            return json.load(file_object)
+
+
+    def getThemeValues(self, element):
+        if self.theme:
+            return self.theme.get('values').get(element)
+        else:
+            return None
+
+    def getStyleSheet(self, element, state='default'):
+        styleSheet = ""
+        elementValues = self.getThemeValues(element)
+        if elementValues:
+            stateValues = elementValues.get(state)
+            if stateValues:
+                for elementVal in stateValues:
+                    styleSheet = styleSheet + f"{elementVal.get('name')}:{elementVal.get('value')};"
+        return styleSheet
+
+    def subscribe(self, element):
+        self.subs.append(element)
+
+    def unsubscribe(self, element):
+        self.subs.remove(element)
+
+    def notify(self):
+        for sub in self.subs:
+            sub.notify()
