83ff5bafb709580cd65ba8546f45082389f68192
cezar-plesu <79512221+cezar-plesu@users.noreply.github.com>
2022-01-05 23:01:24 +0200

    settings update

--- customWidgets/settingsPanel.py
+++ customWidgets/settingsPanel.py
@@ -1,9 +1,17 @@
-from PyQt5.QtCore import Qt, QRect, QPoint, QSize
-from PyQt5.QtWidgets import QFrame, QScrollArea, QWidget, QVBoxLayout, QSpacerItem, QSizePolicy, QLayout, QComboBox
+import json
+import os
+from os import listdir
+from os.path import isfile
+from shlex import join
+
+from PyQt5.QtCore import Qt, QRect, QPoint, QSize, QFileInfo
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QFrame, QScrollArea, QWidget, QVBoxLayout, QSpacerItem, QSizePolicy, QLayout, QComboBox, \
+    QLabel, QTextEdit, QTreeView, QHeaderView
 
 from customWidgets.buttons.iconClickButton import IconClickButton
 from customWidgets.buttons.settingsButton import SettingsButton
-
+from customWidgets.jsonViewer import JsonModel
 
 
 class SettingsPanel(QFrame):
@@ -17,20 +25,61 @@ class SettingsPanel(QFrame):
 
         self.settingsButton = SettingsButton(parent)
         # self.element = SettingElement(self, 642, 251)
+        self.languageText = QLabel(self)
+        self.themeText = QLabel(self)
+        self.customDesignText = QLabel(self)
+        self.titleText = QLabel(self)
 
         self.languageSelect = QComboBox(self)
+        self.themeSelect = QComboBox(self)
+        # self.customEdit = QTextEdit(self)
+
+        self.view = QTreeView(self)
+        self.model = JsonModel(self)
+
+        self.view
 
         self.hide()
-
 
         self.setupUI()
 
     def setupUI(self):
+
+        self.languageText.setGeometry(31, 130, 235, 30)
+        font = QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
+        self.languageText.setFont(font)
+        self.languageText.setText("Language")
+        self.languageText.setObjectName("label")
+
+        font.setPointSize(25)
+
+        self.titleText.setGeometry(241, 32, 235, 45)
+        self.titleText.setFont(font)
+        self.titleText.setText("Settings")
+        self.titleText.setObjectName("label")
+
+        font.setPointSize(18)
+
+        self.themeText.setGeometry(31, 239, 235, 30)
+        self.themeText.setFont(font)
+        self.themeText.setText("Theme")
+        self.themeText.setObjectName("label")
+
+        self.customDesignText.setGeometry(31, 363, 235, 30)
+        self.customDesignText.setFont(font)
+        self.customDesignText.setText("Custom Design")
+        self.customDesignText.setObjectName("label")
+
         self.setWindowFlags(Qt.WindowStaysOnTopHint)
         self.setGeometry(799, 0, 648, 901)
 
         self.cancelButton.setGeometry(QRect(10, 10, 25, 25))
         self.cancelButton.click_signal.connect(self.closeSettings)
+        self.cancelButton.setObjectName("iconClickButton")
 
         self.settingsButton.click_signal.connect(self.openSettings)
         self.settingsButton.setObjectName("settingButton")
@@ -38,10 +87,34 @@ class SettingsPanel(QFrame):
         self.settingsButton.setGeometry(QRect(1405, 384, 188, 59))
         self.settingsButton.setWindowFlags(Qt.WindowStaysOnTopHint)
 
-        self.languageSelect.setGeometry(QRect(64, 170, 515, 48))
+        font.setPointSize(13)
+
+        self.languageSelect.setGeometry(QRect(64, 170, 515, 28))
+        self.languageSelect.setFont(font)
         self.languageSelect.setObjectName("settingsComboBox")
 
+        self.themeSelect.setGeometry(QRect(64, 288, 515, 28))
+        self.themeSelect.setFont(font)
+        self.themeSelect.setObjectName("settingsComboBox")
 
+        self.uploadFiles()
+
+        self.themeSelect.currentTextChanged.connect(lambda: self.themeEventComboBox())
+
+        # self.customEdit.setGeometry(QRect(64, 412, 522, 346))
+        # self.customEdit.setFont(font)
+        # self.customEdit.setObjectName("settingsComboBox")
+
+        self.view.setModel(self.model)
+        json_path = QFileInfo(__file__).absoluteDir().filePath("styles/default.json")
+        with open(json_path) as file:
+            document = json.load(file)
+            self.model.load(document)
+        self.view.header().setSectionResizeMode(0, QHeaderView.Stretch)
+        # self.view.setAlternatingRowColors(True)
+        self.view.resize(522, 346)
+        self.view.move(QPoint(64, 412))
+        self.view.setObjectName("settingsComboBox")
 
     def setSettings(self, settings):
         self.settings = settings
@@ -54,11 +127,14 @@ class SettingsPanel(QFrame):
     def applyStyleSheets(self):
         if self.settings:
             self.settings.applyStylesheet(self)
+            self.settings.applyStylesheet(self.languageText)
+            self.settings.applyStylesheet(self.themeText)
+            self.settings.applyStylesheet(self.customDesignText)
             self.settings.applyStylesheet(self.languageSelect)
-            print(self.languageSelect.styleSheet())
-
-
-
+            self.settings.applyStylesheet(self.themeSelect)
+            self.settings.applyStylesheet(self.view)
+            # self.settings.applyStylesheet(self.customEdit)
+            self.settings.applyStylesheet(self.titleText)
 
     def resizeContent(self, difSize):
         self.resize(difSize.width() + self.size().width(), difSize.height() + self.size().height())
@@ -76,3 +152,25 @@ class SettingsPanel(QFrame):
 
     def notify(self):
         self.applyStyleSheets()
+
+    def uploadFiles(self):
+        mypath = os.path.abspath(os.getcwd()) + "\customWidgets\styles"
+        files = [f for f in listdir(mypath)]
+
+        for file in files:
+            self.themeSelect.addItem(file[:-5])
+
+    def themeEventComboBox(self):
+        self.view.setModel(self.model)
+        json_path = QFileInfo(__file__).absoluteDir().filePath(f"styles/{self.themeSelect.currentText()}.json")
+        with open(json_path) as file:
+            document = json.load(file)
+            self.model.load(document)
+        self.view.header().setSectionResizeMode(0, QHeaderView.Stretch)
+        # self.view.setAlternatingRowColors(True)
+        self.view.resize(522, 346)
+        self.view.move(QPoint(64, 412))
+        self.view.setObjectName("settingsComboBox")
+
+
+        # os.path.abspath(os.getcwd()
